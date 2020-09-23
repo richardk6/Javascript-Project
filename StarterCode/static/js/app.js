bfea3ace11ba21aebcadf9b9e1f2c7c07c77d6af
Kelsey Richardson <kelseyrichardson6@gmail.com>
2020-09-23 10:40:05 -0500

    cleaned up code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,20 +1,19 @@
 // from data.js
 var tableData = data;
 
-// Select the form
-var form = d3.select("#form");
-
-// get a variable for table body
+// Select table
 var tbody = d3.select("tbody");
 
-// create cells and display each cell with ufo data
-function displayData (ufoSights) {
+// function to display UFO sightings
+
+function displayData(ufoSightings) {
     tbody.html("");
-    ufoSights.forEach((ufoRecord) => {
+    ufoSightings.forEach((individualRow) => {
         var row = tbody.append("tr");
-        Object.entries(ufoRecord).forEach(([key, value]) => {
+
+        Object.entries(individualRow).forEach(([key, value]) => {
             var cell = row.append("td");
-            cell.html(value);
+                cell.html(value);
         });
     });
 };
@@ -23,24 +22,12 @@ function displayData (ufoSights) {
 console.log(tableData);
 displayData(tableData);
 
-// tableData.forEach(function(ufoData) {
-//     console.log(ufoData);
-//     var row = tbody.append("tr");
-
-//     Object.entries(ufoData).forEach(function([key, value]) {
-//         console.log(key, value);
-
-//         var cell = row.append("td");
-//         cell.text(value);
-//     });
-// });
-
 // Select the button
 var button = d3.select("#filter-btn");
 
 var inputDate = d3.select("#datetime");
 
-// Complete the event handler function for the form
+// // Complete the event handler function for the form
 function runEnter() {
 
     // Prevent the page from refreshing
@@ -56,13 +43,5 @@ function runEnter() {
 
 };
 
-// Create event handlers for clicking the button or pressing the enter key
+// // Create event handlers for clicking the button or pressing the enter key
 button.on("click", runEnter);
-// displayData(filteredData);
-
-
-//   // Then, select the unordered list element by class name
-//     var date = d3.select(tbody);
-
-//     date.append.text(filteredData);
-// };
